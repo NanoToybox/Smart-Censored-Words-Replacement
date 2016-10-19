22852f53e6c58e9a5d7b6f5925abde28e2eded4e
David <wyatwork@126.com>
2016-10-19 21:05:06 +0800

    get segmented search result from a single input

--- GetFeatures/GetFeatures/pipelines.py
+++ GetFeatures/GetFeatures/pipelines.py
@@ -6,12 +6,22 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import json
 import codecs
+import urllib
+from pyltp import Segmentor
 
 class GetfeaturesPipeline(object):
 	def __init__(self):
-		self.file = codecs.open('test_utf8.json', 'wb', encoding='utf-8')
+		#self.file = codecs.open('test_single.txt', 'wb', encoding='utf-8')
+		self.segmentor = Segmentor()
 
 	def process_item(self, item, spider):
-		line = json.dumps(dict(item)) + '\n'
-		self.file.write(line.decode("unicode_escape"))
+		self.segmentor.load('/Users/David/Applications/ltp_data/cws.model')  # 加载模型
+		words = list(self.segmentor.segment(item["content"].encode("utf-8")) ) # 分词
+		for word in words:
+			self.file.write(word.decode("utf-8"))
+			self.file.write("\n".decode("utf-8"))
 		return item
+
+	def close_spider(self, spider):
+		self.segmentor.release()  # 释放模型
+		self.file.close()
--- GetFeatures/GetFeatures/spiders/c_darwini.py
+++ GetFeatures/GetFeatures/spiders/c_darwini.py
@@ -2,8 +2,8 @@
 import scrapy
 import lxml.etree
 import lxml.html
+import codecs
 from GetFeatures.items import GetfeaturesItem
-
 
 #complexity depends on pv per run
 class C_Darwini(scrapy.Spider):
@@ -11,17 +11,19 @@ class C_Darwini(scrapy.Spider):
 	base_url = "https://cn.bing.com/search?q="
 
 	def start_requests(self):
+		#dirty_words = self.file = codecs.open('test_single.txt', 'wb', encoding='utf-8')
 		urls = [
 			"http://cn.bing.com/search?q=毛泽东"
 		]
 		for url in urls:
-			yield scrapy.Request(url=url, callback=self.parse_baidu)
+			yield scrapy.Request(url=url, callback=self.parse_bing)
 
 	# The unicode <-> str shit below may not be needed. this all shitholes gave me a poor sleep.
 	# Don't want to touch it since then.
-	def parse_baidu(self, response):
+	def parse_bing(self, response):
 		root = lxml.html.fromstring(unicode(response.body, "utf-8"))
 		lxml.etree.strip_elements(root, lxml.etree.Comment, "script", "head", "style")
 		item = GetfeaturesItem()
-		item["content"] = unicode(lxml.html.tostring(root, method="text", encoding="utf-8"), "utf-8").encode("utf-8")
+		item["content"] = ' '.join(i for i in root.itertext())
+		#unicode(lxml.html.tostring(root, method="text", encoding="utf-8"), "utf-8").encode("utf-8")
 		return item
