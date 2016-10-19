6bb2be6d2a1fc43331b78e6be1515b9f134dce39
David <wyatwork@126.com>
2016-10-20 00:21:01 +0800

    阿西吧！TODO: 1) take samples to extract features 2) write into file early then release memory somehow and write a lookup json 3) consider renting a server

--- GetFeatures/GetFeatures/pipelines.py
+++ GetFeatures/GetFeatures/pipelines.py
@@ -8,14 +8,15 @@ from pyltp import Segmentor
 class GetfeaturesPipeline(object):
 	#items refed to print statics in close_spider
 	items = {}
+	total_items = 0
 
 	def __init__(self):
-		self.file = codecs.open('test_multi.json', 'w', encoding='utf-8')
+		self.file = codecs.open('test_multi.json', 'w', encoding='utf-8') #test_multi , raw_results
 		self.segmentor = Segmentor()
 		self.segmentor.load('/Users/David/Applications/ltp_data/cws.model')  # 加载模型
 
 	def process_item(self, item, spider):
-		counter = 0
+		self.total_items += 1
 		#segmenting content of the item
 		item["content"] = list(self.segmentor.segment(item["content"].encode("utf-8")) ) # 分词
 		item["counter"] = len(item["content"])
@@ -26,9 +27,10 @@ class GetfeaturesPipeline(object):
 	def close_spider(self, spider):
 		#dump json file
 		self.file.write(unicode(json.dumps(self.items,  ensure_ascii= False, encoding="utf-8"), "utf-8"))
-		#print static, 52 means no result due to laws
+		#print static, ~52 means no result due to the laws
+		print("total items: " + str(self.total_items))
 		for k,v in self.items.iteritems():
 			print(str(len(v))+ "\t" + k)
-		
+
 		self.segmentor.release()  # 释放模型
 		self.file.close()
--- GetFeatures/GetFeatures/spiders/c_darwini.py
+++ GetFeatures/GetFeatures/spiders/c_darwini.py
@@ -12,7 +12,7 @@ class C_Darwini(scrapy.Spider):
 		base_url = "https://cn.bing.com/search?q=" #https://www.google.com/search?q=
 		urls = {}
 		#reading "dirty words"
-		dirty_words_file = codecs.open('dirtywords_short.txt', 'r', encoding='utf-8')
+		dirty_words_file = codecs.open('dirtywords_short.txt', 'r', encoding='utf-8') 
 		word = dirty_words_file.readline().rstrip(u'\n')
 		#construct urls
 		while(word != u''):
@@ -23,7 +23,6 @@ class C_Darwini(scrapy.Spider):
 			request = scrapy.Request(url=url, callback=self.parse_bing)
 			request.meta['key'] = key
 			yield request
-
 
 	def parse_bing(self, response):
 		#get texts, can be refined here
