8946058138ab28c52c4086210bf17c5b8331d984
David <wyatwork@126.com>
2016-10-21 16:06:56 +0800

    randomly take samples

--- GetFeatures/GetFeatures/Sampler.py
+++ GetFeatures/GetFeatures/Sampler.py
@@ -1,22 +1,25 @@
 #coding=utf-8
 import json
 import codecs
+from random import shuffle
 
 # move words from original txt into sample files
 dirty_words_file = codecs.open('before_sampling.txt', 'r', encoding='utf-8')
-word = dirty_words_file.readline().rstrip(u'\n')
+words = dirty_words_file.readlines()
+shuffle(words)
+
 counter = 0
 sample_counter = 0
 sample = codecs.open('samples/sample_' + str(sample_counter) + ".txt", 'w', encoding='utf-8')
 
-#take samples
-while(word != u''):
-	sample.write(word + u"\n")
+
+#randomly separate samples
+for w in words:
+	sample.write(w)
 	counter += 1
-	while( counter >= 1000 ):
+	if(counter >= 1000):
 		sample_counter += 1
 		counter = 0
 		sample.close()
 		sample = codecs.open('samples/sample_' + str(sample_counter) + ".txt", 'w', encoding='utf-8')
-	word = dirty_words_file.readline().rstrip(u'\n')
 sample.close()
