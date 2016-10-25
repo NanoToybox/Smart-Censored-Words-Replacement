f995fcd437de096d005179ba620f7bc01375b8c1
David <wyatwork@126.com>
2016-10-26 00:14:44 +0800

    apply PCA. TODO 1) case study 2) apply weight to some feat. 3)use larger test set (m>n)

--- K-means/main.m
+++ K-means/main.m
@@ -1,15 +1,30 @@
-%% Initialization
+% Initialization
 clear ; close all; clc
 
 X = load("x_feat_0.txt");
 
-K = 20; 
+% PCA
+[U, S] = pca(X);
+
+dimension = 1000;
+
+% check variance retaining percentage, should be over 0.99
+retain_perc = sum(sum(S(:,1:dimension)))/sum(sum(S));
+fprintf("PCA with dimension : %i, variance retaining percentage : %d\n",dimension,retain_perc);
+
+% Dimension Reduction
+Z = projectData(X, U, dimension);
+
+
+
+
+K = 6; %政治(events people organization) ads(chem guns no) sex(book) abuse  website other 
 max_iters = 10;
 
 % initialize the centroids randomly. 
-initial_centroids = kMeansInitCentroids(X, K);
+initial_centroids = kMeansInitCentroids(Z, K);
 
 % Run K-Means
-[centroids, idx] = runkMeans(X, initial_centroids, max_iters);
+[centroids, idx] = runkMeans(Z, initial_centroids, max_iters);
 
 save out_test.txt idx
