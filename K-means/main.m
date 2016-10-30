b8ccedc5381f2fd06ebd0ade5a140d99acdf7b13
David <wyatwork@126.com>
2016-10-30 23:23:09 +0800

    test result
    Former-commit-id: 55ff0ff515fd72640dac86e537340e2910fa723a

--- K-means/main.m
+++ K-means/main.m
@@ -4,10 +4,12 @@ clear ; close all; clc
 K = 6; %政治(events people organization) ads(chem guns no) sex(book) abuse  website other 
 max_iters = 10;
 
+X = load("input/PCA_training_set.txt").Z;
+
 % initialize the centroids randomly. 
-initial_centroids = kMeansInitCentroids(Z, K);
+initial_centroids = kMeansInitCentroids(X, K);
 
 % Run K-Means
-[centroids, idx] = runkMeans(Z, initial_centroids, max_iters);
+[centroids, idx] = runkMeans(X, initial_centroids, max_iters);
 
-save out_test.txt idx
+save "output/out_test.txt" idx
---
+++ K-means/output/group_result.py
@@ -0,0 +1,36 @@
+#coding=utf-8
+import codecs
+import json
+
+counter = 0;
+
+group_file = codecs.open('group_test.txt', 'w', encoding='utf-8')
+
+x_name = []
+for i in range(0,4):
+	x_name_file = codecs.open('../input/x_'+str(i)+'.txt', 'r', encoding='utf-8')
+	x_name.extend(x_name_file.readlines())
+
+cluster_id_file =  codecs.open('out_test.txt', 'r', encoding='utf-8')
+cluster_id = cluster_id_file.readlines()
+
+cluster = {}
+
+for line in cluster_id:
+	if line[0] != '#' and counter < len(x_name):
+		value = cluster.get(line.rstrip('\n'))
+		if value == None :
+			cluster[line.rstrip('\n')] = [x_name[counter].rstrip('\n')]
+		else: 
+			value.append(x_name[counter].rstrip('\n'))
+		counter += 1
+
+for k,v in cluster.iteritems():
+	print len(v)
+	
+group_file.write(json.dumps(cluster,  ensure_ascii= False, encoding="utf-8"))
+
+group_file.close()
+x_name_file.close()
+cluster_id_file.close()
+
